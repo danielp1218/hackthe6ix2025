a1a46fb3db357829c0457b60e43496bb642edd8a
danielp1218 <danielpu2007@gmail.com>
2025-07-19 05:12:12 -0400

    a

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ screen = pg.display.set_mode((800, 600))
 clock = pg.time.Clock()
 
 # Define the Trapezoid class
+
+
+
 class Trapezoid:
-    def __init__(self, x_offset, y_offset, color, speed_x, speed_y, lane=0):
+
+    def __init__(self, x_offset, y_offset, color, lane=0):
         self.x_offset = x_offset
         self.y_offset = y_offset
         self.color = color
-        self.speed_x = speed_x
-        self.speed_y = speed_y
         self.lane = lane
 
     def draw(self, screen):
@@ -30,13 +32,19 @@ class Trapezoid:
                 (460 + self.x_offset, 50 + self.y_offset),
                 (404, 50 + self.y_offset)
             ])
-        # Update the position of the trapezoid
-        self.x_offset += self.speed_x
-        self.y_offset += self.speed_y
 
 running = True
 
-delay = 0
+NOTE_SPEED = 2
+LINE_SLOPE = 0.25
+
+notes = [
+    {'x': 0, 'color': (255, 0, 0), 'time':0, 'lane': 1},
+    {'x': 0, 'color': (0, 255, 0), 'time':1, 'lane': 0},
+    {'x': 0, 'color': (0, 0, 255), 'time':2, 'lane': 1}
+]
+
+active_notes = []
 
 while running:
     for event in pg.event.get():
@@ -49,25 +57,19 @@ while running:
     pg.draw.line(screen, (0, 255, 0), (350, 0), (200, 600), 5)
     pg.draw.line(screen, (0, 255, 0), (450, 0), (600, 600), 5)
 
-    # Create and update the trapezoid instance outside the loop
-    if 'trapezoid' not in locals():
-        trapezoid = Trapezoid(x_offset=0, y_offset=0, color=(255, 0, 0), speed_x=0.25, speed_y=1, lane=1)
-    
-    # Draw and update the trapezoid position
-    trapezoid.draw(screen)
+    while len(notes) > 0 and notes[0]['time'] <= pg.time.get_ticks() / 1000:
+        active_notes.append(Trapezoid(notes[0]['x'], 0, notes[0]['color'], notes[0]['lane']))
+        print("popping note:", notes[0])
+        notes.pop(0)
 
-    delay += 1
-
-    xinR = 0
-    yinR = 0
-
-    if delay > 150:
-        if 'trapezoid2' not in locals():
-            trapezoid2 = Trapezoid(x_offset=0, y_offset=0, color=(255, 0, 0), speed_x=0.25, speed_y=1, lane=0)
-        trapezoid2.draw(screen)
-
+    for note in active_notes:
+        note.draw(screen)
+        print("drawing note at x_offset:", note.x_offset, "y_offset:", note.y_offset, "lane:", note.lane)
+        note.x_offset += LINE_SLOPE * NOTE_SPEED
+        note.y_offset += NOTE_SPEED
+        
+    pg.display.flip()
     clock.tick(60)
-    pg.display.flip()       # Update the display
 
 pg.quit()
 
