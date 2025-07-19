ab94bc785e7d8941366064292229361fa22f2910
danielp1218 <danielpu2007@gmail.com>
2025-07-19 02:26:44 -0400

    init

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-print("hello world")
+import pygame as pg
+
+pg.init()
+screen = pg.display.set_mode((800, 600))
+clock = pg.time.Clock()
+
+running = True
+
+bullets = [[0, 50], [0, 100], [0, 200]]
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0)) 
+
+    for b in bullets:
+        b[0] += 1
+        pg.draw.circle(screen, (255, 0, 0), (b[0], b[1]), 5)
+
+    clock.tick(60)
+    pg.display.flip()       # Update the display
+
+pg.quit()
 
